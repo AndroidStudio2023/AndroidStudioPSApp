3227022e37e8a5b52fb1403d972ed60ea6add6ad
George Denesidis <georgios.denesidis@gmail.com>
2023-04-18 17:13:58 +0300

    1stPageDes2

--- app/src/main/java/com/example/physiotherapycenterapplication/MainActivity.java
+++ app/src/main/java/com/example/physiotherapycenterapplication/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.physiotherapycenterapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void moveToLoginPage(View view){
+        String userType;
+        login = (Button)view;
+
+        userType = login.getText().toString();
+
+        Toast.makeText(this, userType, Toast.LENGTH_SHORT).show();
     }
 }
