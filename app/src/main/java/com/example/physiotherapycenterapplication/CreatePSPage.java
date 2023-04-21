48834a2588d8890c25ab58c2961f0c4bdb9dfe96
George Denesidis <georgios.denesidis@gmail.com>
2023-04-22 00:51:53 +0300

    DrawCreateNewPSActivityT1

--- app/src/main/java/com/example/physiotherapycenterapplication/CreatePSPage.java
+++ app/src/main/java/com/example/physiotherapycenterapplication/CreatePSPage.java
@@ -3,6 +3,11 @@ package com.example.physiotherapycenterapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class CreatePSPage extends AppCompatActivity {
 
@@ -10,5 +15,17 @@ public class CreatePSPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_pspage);
+    }
+
+    public void CreateNewPS(View view){
+        TextView name = findViewById(R.id.newPSName);
+        TextView address = findViewById(R.id.newPSAddress);
+        TextView afm = findViewById(R.id.newPSAFM);
+
+        String aName = name.getText().toString();
+        String aAddress = address.getText().toString();
+        String aAFM = afm.getText().toString();
+
+        Toast.makeText(this, "Name: "+aName+"\nAddress: "+aAddress+"\nAFM: "+aAFM, Toast.LENGTH_SHORT).show();
     }
 }
