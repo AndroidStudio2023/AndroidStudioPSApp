8065814a145378018aa9d4926654a6767fd2af63
George Denesidis <georgios.denesidis@gmail.com>
2023-04-22 00:22:51 +0300

    DrawPSFCentralPageT2 and connect with CreatePSPage Activity + LoginPage Activity

--- app/src/main/java/com/example/physiotherapycenterapplication/PSFCentralPage.java
+++ app/src/main/java/com/example/physiotherapycenterapplication/PSFCentralPage.java
@@ -2,13 +2,38 @@ package com.example.physiotherapycenterapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class PSFCentralPage extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_psfcentral_page);
+    }
+
+    //Analoga me to koumpi pou epilegei o xristis
+    //Ton pigainei stin antoistixi selida
+    public void LogOutORnextPage(View view){
+        //get button name
+        Button aButton= (Button)view;
+        String buttonName = aButton.getText().toString();
+        Intent intent;
+
+        if(buttonName.equals("Νέο Φυσικοθεραπευτήριο")){
+            intent = new Intent(getApplicationContext(),CreatePSPage.class);
+            startActivity(intent);
+        } else if (buttonName.equals("Log Out")) {
+            intent = new Intent(getApplicationContext(),LoginPageActivity.class);
+            intent.putExtra("TypeUser","ΠΣΦ");
+            startActivity(intent);
+        }else{
+            Toast.makeText(this, "Coming Soon!!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
