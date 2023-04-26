d2f501bef2243eff027a15f70ec18703fc228430
George Denesidis <georgios.denesidis@gmail.com>
2023-04-26 17:42:40 +0300

    Add xml files for TextFileds borders (DrawblesFolder) and complete LoginPage

--- app/src/main/java/com/example/physiotherapycenterapplication/LoginPageActivity.java
+++ app/src/main/java/com/example/physiotherapycenterapplication/LoginPageActivity.java
@@ -11,6 +11,8 @@ import android.widget.Toast;
 
 import com.example.physiotherapycenterapplication.R.id;
 
+import org.w3c.dom.Text;
+
 public class LoginPageActivity extends AppCompatActivity {
 
     TextView user;
@@ -31,7 +33,12 @@ public class LoginPageActivity extends AppCompatActivity {
 
     public void goToUserCentralPage(View view){
 
-
+        EditText someField;
+        //"Keys" variables
+        boolean correctEmail,correctPassword;
+        //Find and Save ErrorMessages
+        TextView wrongEmailMessage = findViewById(R.id.emailErrorMessage);
+        TextView wrongPassMessage = findViewById(R.id.passwordErrorMessage);
         //Find and save email
         EditText Email = findViewById(id.userEmail);
         String userEmail = Email.getText().toString();
@@ -41,17 +48,62 @@ public class LoginPageActivity extends AppCompatActivity {
 
         //Elegxos dedomenwn xristi
         //!Pros to paron "uparxei" 1 xristis, o PSF
-        if(typeUser.equals("ΠΣΦ") && userEmail.equals("psf@gmail.com") && userPassword.equals("1234")){
+
+        correctEmail = checkEmail(typeUser,userEmail);
+        correctPassword = checkPassword(typeUser,userPassword);
+        //1o senario: Lathos email
+        someField = findViewById(id.userEmail);
+        if(!correctEmail){
+            someField.setBackground(getResources().getDrawable(R.drawable.error_textfileds_border));
+            wrongEmailMessage.setText("! Email is incorrect or empty");
+        }else{
+            someField.setBackground(getResources().getDrawable(R.drawable.normal_textfileds_border));
+            wrongEmailMessage.setText("");
+        }
+
+        //2o senario: Lathos paswword
+        someField = findViewById(id.userPassword);
+        if(!correctPassword){
+            someField.setBackground(getResources().getDrawable(R.drawable.error_textfileds_border));
+            wrongPassMessage.setText("! Password is incorrect or empty");
+        }else{
+            someField.setBackground(getResources().getDrawable(R.drawable.normal_textfileds_border));
+            wrongPassMessage.setText("");
+        }
+
+        //Katharisma formas
+        Email.setText("");
+        Password.setText("");
+
+        //3o senario: Swsta stoixeia
+        if(correctEmail && correctPassword){
             //Katharisma formas
             Email.setText("");
             Password.setText("");
             //Sundesi stin kentrikh selida PSF
             Intent intent = new Intent(getApplicationContext(),PSFCentralPage.class);
             startActivity(intent);
-        }else{
-            Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
         }
 
 
+
+    }
+
+    public boolean checkEmail(String type,String email){
+
+        if(type.equals("ΠΣΦ") && email.equals("psf@gmail.com")){
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+
+    public boolean checkPassword(String type, String password){
+        if(type.equals("ΠΣΦ") && password.equals("1234")){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
