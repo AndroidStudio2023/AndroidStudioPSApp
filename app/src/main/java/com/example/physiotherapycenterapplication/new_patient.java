5d29cf2b6b3ee7a7f27e8cdd7358f4b12ac902f9
GeorgeDiskos <103891202+GeorgeDiskos@users.noreply.github.com>
2023-05-08 16:41:22 +0300

    Main doctor page and new patient activites

---
+++ app/src/main/java/com/example/physiotherapycenterapplication/new_patient.java
@@ -0,0 +1,14 @@
+package com.example.physiotherapycenterapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class new_patient extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_patient);
+    }
+}
