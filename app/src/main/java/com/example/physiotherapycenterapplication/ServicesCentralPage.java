941460ad5d35ac54a6ca515a50829d2f2e52dfbc
George Denesidis <georgios.denesidis@gmail.com>
2023-05-09 15:32:45 +0300

    Central Services Page import

---
+++ app/src/main/java/com/example/physiotherapycenterapplication/ServicesCentralPage.java
@@ -0,0 +1,14 @@
+package com.example.physiotherapycenterapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ServicesCentralPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_services_central_page);
+    }
+}
