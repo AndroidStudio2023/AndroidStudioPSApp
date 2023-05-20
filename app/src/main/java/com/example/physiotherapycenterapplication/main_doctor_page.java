b508a0f968ef5a1ca409a15a4cd93e335f98e65f
GeorgeDiskos <103891202+GeorgeDiskos@users.noreply.github.com>
2023-05-20 12:35:38 +0300

    buttons

--- app/src/main/java/com/example/physiotherapycenterapplication/main_doctor_page.java
+++ app/src/main/java/com/example/physiotherapycenterapplication/main_doctor_page.java
@@ -3,13 +3,40 @@ package com.example.physiotherapycenterapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.widget.Button;
+import android.view.View;
+
+
+
 
 public class main_doctor_page extends AppCompatActivity {
 
+    public Button buttona;
+    public Button buttonb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_doctor_page);
+
+        buttona = (Button) findViewById(R.id.button5);
+        buttonb = (Button) findViewById(R.id.button4);
+
+
+        buttona.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(main_doctor_page.this,new_patient.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               // Intent intent = new  Intent(main_doctor_page.this,)
+            }
+        });
     }
 
 
