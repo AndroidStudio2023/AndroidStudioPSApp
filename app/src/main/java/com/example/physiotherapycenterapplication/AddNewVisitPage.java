661670b6387583aaa7a7f12b2db892c083eb9474
George Denesidis <georgios.denesidis@gmail.com>
2023-06-06 12:18:46 +0300

    Add Fwtios' Page

---
+++ app/src/main/java/com/example/physiotherapycenterapplication/AddNewVisitPage.java
@@ -0,0 +1,14 @@
+package com.example.physiotherapycenterapplication;
+//This activity create by Fwtios
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddNewVisitPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_visit_page);
+    }
+}
