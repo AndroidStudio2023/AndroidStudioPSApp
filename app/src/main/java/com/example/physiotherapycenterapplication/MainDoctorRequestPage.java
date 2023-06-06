d96d63ac4acdcf0c1e2773fbc404df0bb8d018e8
George Denesidis <georgios.denesidis@gmail.com>
2023-06-06 12:10:32 +0300

    Add Iliana's Page

---
+++ app/src/main/java/com/example/physiotherapycenterapplication/MainDoctorRequestPage.java
@@ -0,0 +1,16 @@
+package com.example.physiotherapycenterapplication;
+//Main page with patients request for physiotherapist
+//Create by Iliana
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainDoctorRequestPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_doctor_request_page);
+    }
+}
